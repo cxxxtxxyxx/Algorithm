bdc26e397caf1708913632a99403df4872271d85
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-01-04 16:46:15 +0900

    [Silver II] Title: 부분수열의 합, Time: 188 ms, Memory: 116996 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,18 +1,23 @@
-import sys
-from itertools import combinations
-
-input = sys.stdin.readline
-
 n, s = list(map(int, input().split()))
 
 input_nums = list(map(int, input().split()))
 input_nums.sort()
 count = 0
 
-for i in range(1, n + 1):
-    comb = list(combinations(input_nums, i))
-    for items in comb:
-        if sum(items) == s:
-            count += 1
 
+def getSequence(idx, _sum):
+    global count
+    if idx >= n:
+        return
+
+    _sum += input_nums[idx]
+
+    if _sum == s:
+        count += 1
+    
+    getSequence(idx + 1, _sum)
+    getSequence(idx + 1, _sum - input_nums[idx])
+    
+
+getSequence(0, 0)
 print(count)
