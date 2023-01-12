32b7185b1f4b943fafc53f255c8eae95d70b900c
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-01-12 18:20:30 +0900

    [Silver II] Title: 에디터, Time: 332 ms, Memory: 175528 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,72 +1,34 @@
 import sys
+import copy
 
-"""
-총 커서 위치 => 길이 L 기준 L + 1
-L 커서를 왼쪽으로 한칸 옮김 (문장 맨 앞이면 무시)
-R 커서를 오른쪽으로 한칸 옮김 (문장 맨 뒤면 무시)
-B 커서 왼쪽 문자를 지움(문장 맨 앞이면 무시) (커서 위치 그대로)
-P $ $문자를 커서 왼쪽에 추가함
-"""
+input = sys.stdin.readline
 
-# testCase = list(input().replace('\n', ''))
-# cursor = len(testCase)
-# n = int(input())
-# for i in range(n):
-#   app = sys.stdin.readline().replace('\n', '').split(' ')
-  
-#   if app[0] == 'L':
-#     if cursor != 0:
-#       cursor -= 1
+LEFT = "L"
+RIGHT = "D"
+DELETE = "B"
+WRITE = "P"
 
-    
-#   elif app[0] == 'D':
-#     if cursor != len(testCase):
-#       cursor += 1
+input_str = list(input().strip())
 
-    
-#   elif app[0] == 'B':
-#     if cursor != 0:
-#       if cursor == len(testCase):
-#         del testCase[cursor - 1]
-#         cursor = len(testCase)
+left_cursor = copy.deepcopy(input_str)
+right_cursor = []
 
-#       else:
-#         del testCase[cursor - 1]
-#         cursor -= 1
+m = int(input().strip())
 
-    
-    
-#   else:
-#     testCase.insert(cursor, app[1])
-#     cursor += 1
+for __ in range(m):
+    operator = input().strip().split()
 
-# print(''.join(testCase))
+    if operator[0] == LEFT:
+        if len(left_cursor) != 0:
+            right_cursor.append(left_cursor.pop())
+    elif operator[0] == RIGHT:
+        if len(right_cursor) != 0:
+            left_cursor.append(right_cursor.pop())
+    elif operator[0] == DELETE:
+        if len(left_cursor) != 0:
+            left_cursor.pop()
+    else:
+        left_cursor.append(operator[1])
 
-#-------------------------
-
-# 커서를 기준으로 양쪽을 스택으로 분해
-# 마지막에 다시 합치기
-
-st1 = list(input().replace('\n', ''))
-st2 = []
-n = int(input())
-for i in range(n):
-  app = sys.stdin.readline().replace('\n', '').split(' ')
-  if app[0] == 'L':
-    if st1:
-      st2.append(st1.pop())
-  
-  elif app[0] == 'D':
-    if st2:
-      st1.append(st2.pop())
-  
-  elif app[0] == 'B':
-    if st1:
-      st1.pop()
-  
-  else:
-    st1.append(app[1])
-    
-
-st1.extend(reversed(st2))
-print(''.join(st1))
+right_cursor.reverse()
+print("".join(left_cursor + right_cursor))
