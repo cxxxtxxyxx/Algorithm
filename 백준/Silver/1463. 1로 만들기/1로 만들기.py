6d0d6bad03e71ae588f5b3f1264e97b6acf5f118
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-01-26 18:10:49 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 122304 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -4,37 +4,43 @@ input = sys.stdin.readline
 
 n = int(input())
 
-res = [0] * 1000001
+res = [0] * (n + 1)
 
-res[1] = 0
-res[2] = 1
-res[3] = 1
-res[4] = 2
 
-for i in range(5, 1000001):
-    _min = 0
-    if i % 3 == 0 and i % 2 == 0:
-        _min = min(res[i // 3], res[i // 2], res[i - 1])
-        if _min == res[i // 3]:
-            res[i] = 1 + res[i // 3]
-        elif _min == res[i // 2]:
-            res[i] = 1 + res[i // 2]
-        else:
-            res[i] = 1 + res[i - 1]
-    elif i % 3 == 0 and i % 2 != 0:
-        _min = min(res[i // 3], res[i - 1])
-        if _min == res[i // 3]:
-            res[i] = 1 + res[i // 3]
-        else:
-            res[i] = 1 + res[i - 1]
-    elif i % 2 == 0 and i % 3 != 0:
-        _min = min(res[i // 2], res[i - 1])
-        if _min == res[i // 2]:
-            res[i] = 1 + res[i // 2]
-        else:
-            res[i] = 1 + res[i - 1]
-    else:
-        res[i] = 1 + res[i - 1]
+# for i in range(5, n + 1):
+#     _min = 0
+#     if i % 3 == 0 and i % 2 == 0:
+#         _min = min(res[i // 3], res[i // 2], res[i - 1])
+#         if _min == res[i // 3]:
+#             res[i] = 1 + res[i // 3]
+#         elif _min == res[i // 2]:
+#             res[i] = 1 + res[i // 2]
+#         else:
+#             res[i] = 1 + res[i - 1]
+#     elif i % 3 == 0 and i % 2 != 0:
+#         _min = min(res[i // 3], res[i - 1])
+#         if _min == res[i // 3]:
+#             res[i] = 1 + res[i // 3]
+#         else:
+#             res[i] = 1 + res[i - 1]
+#     elif i % 2 == 0 and i % 3 != 0:
+#         _min = min(res[i // 2], res[i - 1])
+#         if _min == res[i // 2]:
+#             res[i] = 1 + res[i // 2]
+#         else:
+#             res[i] = 1 + res[i - 1]
+#     else:
+#         res[i] = 1 + res[i - 1]
 
+
+for i in range(2, n + 1):
+    res[i] = res[i - 1] + 1
+
+    if i % 3 == 0:
+        res[i] = min(res[i], res[i // 3] + 1)
+    if i % 2 == 0:
+        res[i] = min(res[i], res[i // 2] + 1)
+
+    
 
 print(res[n])
