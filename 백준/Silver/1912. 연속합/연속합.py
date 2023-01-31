8c88700059db8627c8c8877001b6b41f5fd04919
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-01-31 16:38:46 +0900

    [Silver II] Title: 연속합, Time: 144 ms, Memory: 127224 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,14 +1,14 @@
 import sys
-import copy
+
 input = sys.stdin.readline
 
 n = int(input())
 
 nums = list(map(int, input().split()))
 
+dp = [nums[0]] + [0] * (n - 1)
 
 for i in range(1, n):
-   nums[i] = max(nums[i], nums[i] + nums[i - 1])
+    dp[i] = max(nums[i], dp[i - 1] + nums[i])
 
-print(max(nums))
-
+print(max(dp))
