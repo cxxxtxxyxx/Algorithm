2f2dc27271e16d531a6ec56d3ffb4b38052cfa4f
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-01-31 17:08:42 +0900

    [Silver I] Title: 카드 구매하기, Time: 128 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -4,12 +4,14 @@ input = sys.stdin.readline
 
 n = int(input())
 
-nums = [0] + list(map(int, input().split()))
+card = [0] + list(map(int, input().split()))
+
 dp = [0] * (n + 1)
 
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        dp[i] = max(dp[i], dp[i - j] + nums[j])
+dp[1] = card[1]
+
+for i in range(2, n + 1):
+    for j in range(i + 1):
+        dp[i] = max(dp[i - j] + card[j], card[i], dp[i])
 
 print(dp[n])
-
