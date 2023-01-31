2cd6cb682b59ef9445720c2aa44cd8c966738c9a
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-01-31 16:46:59 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,21 +1,23 @@
 import sys
 
 input = sys.stdin.readline
+
 n = int(input())
 
+dp = [[0 for _ in range(10)] for __ in range(n + 1)]
 
-res = [[0 for i in range(10)] for __ in range(101)]
+dp[1][0] = 0
 
 for i in range(1, 10):
-    res[1][i] = 1
+    dp[1][i] = 1
 
 for i in range(2, n + 1):
     for j in range(10):
         if j == 0:
-            res[i][j] = res[i - 1][1]
+            dp[i][j] = dp[i - 1][1]
         elif j == 9:
-            res[i][j] = res[i - 1][8]
+            dp[i][j] = dp[i - 1][8]
         else:
-            res[i][j] = res[i - 1][j - 1] + res[i - 1][j + 1]
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
 
-print(sum(res[n]) % 1000000000)
+print(sum(dp[n]) % 1000000000)
