250b134d86a2fda7db1e21d71b09628fa7400f2e
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-02-06 17:39:54 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,22 +1,27 @@
 def solution(n, k):
-    stack = []
+    
+    _count = 0
+    digit_num = []
+
     temp = n
-    while temp != 0:
-        stack.append(temp % k)
-        temp = temp // k
-    stack.reverse()
+    while temp > 0:
+        digit_num.append(temp % k)
+        temp //= k
+    digit_num.reverse()
+
+    _digit = "".join(list(map(str, digit_num)))
     
-    res_str = "".join(list(map(str, stack))).split("0")
-    
-    return len(list(filter(lambda x: x != '' and isPrime(int(x)), res_str)))
+
+    _count = len(list(filter(lambda x: x != '' and isPrime(int(x)), _digit.split("0"))))
+
+    return _count
 
 def isPrime(num):
     if num == 1:
         return False
 
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, int(num**0.5)+1):
         if num % i == 0:
             return False
-
+    
     return True
-
