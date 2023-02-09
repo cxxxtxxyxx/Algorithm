450babf5b1cb0ca334f906070fc5e3f64f5d041c
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-02-09 15:17:28 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 268 ms, Memory: 118448 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -36,20 +36,42 @@ dy = [-1, 1, 0, 0]
 #                 q.append((nx, ny))
 #                 graph[nx][ny] = min(graph[nx][ny], graph)
 
+# def dijkstra(i, j):
+#     global visited
+#     global distance
+#     global graph
+#     q = []
+
+#     visited[i][j] = True
+#     heapq.heappush(q, (graph[i][j], i, j))
+
+#     while q:
+#         cost, x, y = heapq.heappop(q)
+
+#         # if distance[x][y] < cost:
+#         #     continue
+
+#         for k in range(4):
+#             nx = dx[k] + x
+#             ny = dy[k] + y
+
+#             if not (0 <= nx < n and 0 <= ny < n):
+#                 continue
+
+#             new_cost = cost + graph[nx][ny]
+#             if visited[nx][ny] == False:
+#                 distance[nx][ny] = new_cost
+#                 heapq.heappush(q, (distance[nx][ny], nx, ny))
+#                 visited[nx][ny] = True
 def dijkstra(i, j):
-    global visited
-    global distance
-    global graph
     q = []
 
-    visited[i][j] = True
+    distance[i][j] = 0
+
     heapq.heappush(q, (graph[i][j], i, j))
 
     while q:
         cost, x, y = heapq.heappop(q)
-
-        # if distance[x][y] < cost:
-        #     continue
 
         for k in range(4):
             nx = dx[k] + x
@@ -59,11 +81,9 @@ def dijkstra(i, j):
                 continue
 
             new_cost = cost + graph[nx][ny]
-            if visited[nx][ny] == False:
+            if new_cost < distance[nx][ny]:
                 distance[nx][ny] = new_cost
                 heapq.heappush(q, (distance[nx][ny], nx, ny))
-                visited[nx][ny] = True
-
 
 
 case_count = 1
@@ -77,7 +97,7 @@ while True:
 
     visited = [[False] * n for __ in range(n)]
 
-    distance = copy.deepcopy(graph)
+    distance = [[INF] * n for __ in range(n)]
     dijkstra(0, 0)
     print(f'Problem {case_count}: {distance[n - 1][n - 1]}')
     case_count += 1
