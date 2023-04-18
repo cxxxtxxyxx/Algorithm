e0f3c0a7e72abe613df2c36e416eced777f4cea0
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-18 15:56:09 +0900

    [Gold IV] Title: 도둑, Time: 312 ms, Memory: 160096 KB -BaekjoonHub

--- 백준/Gold/13422. 도둑/도둑.py
+++ 백준/Gold/13422. 도둑/도둑.py
@@ -6,37 +6,37 @@ T = int(input())
 
 for __ in range(T):
 
-    N, M, K = map(int, input().strip().split())
+    N, M, K = map(int, input().split())
 
-    home = list(map(int, input().strip().split()))
+    homes = list(map(int, input().split()))
+
+    start, end = 1, M - 1
 
     res = 0
 
-    start, end = 0, M - 1
-    prev = 0
-    _sum = sum(home[start:end + 1])
-
-    if M == N:
-        if sum(home) < K:
-            print(1)
-        else:
-            print(0)
-
-        continue
+    _sum = sum(homes[:end+1])
+    if _sum < K:
+        res += 1
 
     while start < N:
 
-        prev = home[start]
-
-        start += 1
-        end += 1
-
-        _sum -= prev
-        _sum += home[end % N]
+        _sum -= homes[start - 1]
+        _sum += homes[(end + 1) % N]
 
         if _sum < K:
             res += 1
 
-        
+        start += 1
+        end += 1
 
-    print(res)
+    
+    if M == N:
+        if res != 0:
+            print(1)
+        else:
+            print(0)
+
+    else:
+        print(res)
+
+
