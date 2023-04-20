21299cfca40aa6cf24d9c236187c512576ce1c7a
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-20 09:45:16 +0900

    [level 2] Title: 숫자의 표현, Time: 1.99 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,31 @@
+def solution(n):
+    answer = 0
+    
+    # step1: start, end 설정
+    if n == 1:
+        return 1
+    start, end = 1, 2
+    
+    # step2: end를 늘려가며 합을 구함 n을 넘으면 start를 늘림 
+    
+    _sum = start
+    count = 0
+    while end <= n:
+        _sum += end
+        
+        
+            
+        if _sum < n:
+            end += 1
+            
+        else:
+            if _sum == n:
+                count += 1
+                end += 1
+            else:
+                _sum -= start
+                start += 1
+                _sum -= end
+            
+        
+    return count
