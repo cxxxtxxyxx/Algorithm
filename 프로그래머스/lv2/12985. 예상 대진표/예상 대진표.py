eefd0b68514af8ab1511a6869ff62a0b3e6db71c
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-24 13:43:50 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,23 +1,12 @@
-def next_number(num):
-    if num % 2 == 0:
-        return num // 2
-    
-    return num // 2 + 1
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
 
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
-def solution(n,a,b):
-    
-    answer = 0
-
-    # step 1 홀수 // 2 + 1, 짝수 // 2 => 다음 라운드 번호
-    
-    while True:
-        answer += 1
-        
-        if max(a, b) - min(a, b) == 1 and max(a, b) // 2 == min(a, b) // 2 + 1:
-            return answer
-        
-        a = next_number(a)
-        b = next_number(b)
-
-    return answer
+        return answer;
+    }
+}
