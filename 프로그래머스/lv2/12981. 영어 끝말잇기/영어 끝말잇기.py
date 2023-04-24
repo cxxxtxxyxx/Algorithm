8a0fee49926aa6b8425de1f72c14bb2d33f78cb3
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-24 13:31:50 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -3,13 +3,22 @@ def solution(n, words):
 
     cwords = set()
     
+    flag = False
     for idx, word in enumerate(words):
+        
+        if len(word) == 1:
+            flag = True
+            return [idx % n + 1, len(words) // n]
+        
         if word in cwords:
-            return [idx % n + 1, idx // n + 1]
+            flag = True
+            return [idx % n + 1, len(words) // n]
         
         if idx != 0 and words[idx - 1][-1] != word[0]:
-            return [idx % n + 1, idx // n + 1]
+            flag = True
+            return [idx % n + 1, len(words) // n]
         
         cwords.add(word)
         
-    return [0, 0]
+    if flag == False:
+        return [0, 0]
