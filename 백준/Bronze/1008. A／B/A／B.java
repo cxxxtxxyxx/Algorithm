84594bc5c86c0e9014cbda05b4bb5107085b1fa2
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-27 21:07:43 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextFloat();
+        double b = sc.nextFloat();
+
+        System.out.println(a / b);
+    }
+}
