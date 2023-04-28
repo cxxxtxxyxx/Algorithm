7b174d2e21cca6b6b79a43ceed981d04b02fe487
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-28 13:55:55 +0900

    [unrated] Title: 귤 고르기, Time: 55.27 ms, Memory: 17.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+import sys
+
+def solution(k, tangerine):
+    # 1 2 2 3 3 4 5 5 
+    # step 1: len(tangerine) - k 만큼 제외한 귤의 종류가 최소가 되도록
+    count_dict = defaultdict(int)
+    
+    for el in tangerine:
+        count_dict[el] += 1
+    
+    v = list(count_dict.values())
+    v.sort()
+    start = 0
+    end = 0
+    _sum = v[start]
+    count = sys.maxsize
+    
+    while start <= end and end < len(v):
+        if _sum >= k:
+            count = min(count, end - start + 1)
+            _sum -= v[start]
+            start += 1
+            continue
+            
+        if end + 1 < len(v):
+            end += 1
+            _sum += v[end]
+            continue
+        else:
+            break
+
+    return count
