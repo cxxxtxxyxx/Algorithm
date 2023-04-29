bd5a4727234524520038c5644e03a6b6ae54108e
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-29 12:22:09 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def solution(s):
+    q = deque(list(s))
+    answer = is_correct(q)
+    
+    for __ in range(1, len(q)):
+        q.rotate(-1)
+        answer += is_correct(list(q))
+    
+    return answer
+
+def is_correct(str):
+    stack = []
+    open = ["[", "(", "{"]
+    _dict = dict()
+    _dict["]"] = "["
+    _dict[")"] = "("
+    _dict["}"] = "{"
+    
+    
+    for ch in str:
+        
+        if not stack:
+            stack.append(ch)
+            continue
+            
+        if ch in open:
+            stack.append(ch)
+            continue
+            
+        else:
+            if stack[-1] == _dict[ch]:
+                stack.pop()
+                continue
+            return 0
+
+    if stack:
+        return 0
+    return 1
+        
+            
+        
+        
