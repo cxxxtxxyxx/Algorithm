e47ffefd526bbcf6871e49f7179103c4bc3198bd
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-29 12:29:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,44 +1,66 @@
+# from collections import deque
+
+
+# def solution(s):
+#     q = deque(list(s))
+#     answer = is_correct(q)
+    
+#     for __ in range(1, len(q)):
+#         q.rotate(-1)
+#         answer += is_correct(list(q))
+    
+#     return answer
+
+# def is_correct(str):
+#     stack = []
+#     open = ["[", "(", "{"]
+#     _dict = dict()
+#     _dict["]"] = "["
+#     _dict[")"] = "("
+#     _dict["}"] = "{"
+    
+    
+#     for ch in str:
+        
+#         if not stack:
+#             stack.append(ch)
+#             continue
+            
+#         if ch in open:
+#             stack.append(ch)
+#             continue
+            
+#         else:
+#             if stack[-1] == _dict[ch]:
+#                 stack.pop()
+#                 continue
+#             return 0
+
+#     if stack:
+#         return 0
+#     return 1
+        
+
 from collections import deque
+
 
 def solution(s):
     q = deque(list(s))
-    answer = is_correct(q)
+    answer = is_correct(s)
     
     for __ in range(1, len(q)):
         q.rotate(-1)
-        answer += is_correct(list(q))
+        answer += is_correct(''.join(q))
     
     return answer
 
-def is_correct(str):
-    stack = []
-    open = ["[", "(", "{"]
-    _dict = dict()
-    _dict["]"] = "["
-    _dict[")"] = "("
-    _dict["}"] = "{"
-    
-    
-    for ch in str:
-        
-        if not stack:
-            stack.append(ch)
-            continue
-            
-        if ch in open:
-            stack.append(ch)
-            continue
-            
+def is_correct(s):
+    while True:
+        if "[]" in s:
+            s = s.replace("[]", "")
+        elif "()" in s:
+            s = s.replace("()", "")
+        elif "{}" in s:
+            s = s.replace("{}", "")
         else:
-            if stack[-1] == _dict[ch]:
-                stack.pop()
-                continue
-            return 0
-
-    if stack:
-        return 0
-    return 1
-        
-            
-        
-        
+            return 0 if s else 1
