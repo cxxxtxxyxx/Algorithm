30411e977617dcd34dc2bb9aef0958fd28a32cb8
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-01 13:09:34 +0900

    [level 2] Title: 튜플, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -26,7 +26,6 @@ s를 이차원 배열로 만들면 됨
 def solution(s):
 
     s = Counter(re.findall('\d+', s))
-    print(sorted(s.items(), key=lambda x: x[1], reverse=True))
     return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
 
 import re
