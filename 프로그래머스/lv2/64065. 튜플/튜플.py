09a0d8bd7d2c2a9b60cf8b96c1f63b1cb9078b52
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-01 13:04:34 +0900

    [level 2] Title: 튜플, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,25 @@
+"""
+튜플
+1. 중복 원소 ok
+2. 순서가 있음
+3. 원소 개수 유한
+
+s = 원소 개수 n, 중복 원소 없는 튜플
+
+
+s를 이차원 배열로 만들면 됨
+
+길이 짧은 순으로 sort, push
+"},{"로 split, 
+"""
+def solution(s):
+    answer = []
+    res = list(map(lambda x: x.split(","), s[2:-2].split("},{")))
+    res.sort(key=lambda x: len(x))
+    
+    for case in res:
+        for ch in case:
+            if ch not in answer:
+                answer.append(ch)
+    
+    return list(map(int, answer))
