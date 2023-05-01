3d54c48b71737e815e4e2735cb1015c0619de1ba
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-01 13:08:46 +0900

    [level 2] Title: 튜플, Time: 0.12 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -12,13 +12,22 @@ s를 이차원 배열로 만들면 됨
 길이 짧은 순으로 sort, push
 "},{"로 split, 
 """
-def solution(s):
-    answer = []
-    res = list(map(lambda x: x.split(","), s[2:-2].split("},{")))
-    res.sort(key=len)    
-    for case in res:
-        for ch in case:
-            if ch not in answer:
-                answer.append(ch)
+# def solution(s):
+#     answer = []
+#     res = list(map(lambda x: x.split(","), s[2:-2].split("},{")))
+#     res.sort(key=len)    
+#     for case in res:
+#         for ch in case:
+#             if ch not in answer:
+#                 answer.append(ch)
     
-    return list(map(int, answer))
+#     return list(map(int, answer))
+
+def solution(s):
+
+    s = Counter(re.findall('\d+', s))
+    print(sorted(s.items(), key=lambda x: x[1], reverse=True))
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
+
+import re
+from collections import Counter
