b20721754a052dbcd9ea31ef1c45d92b36a2b7bc
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-07 16:07:48 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,11 +1,26 @@
-import re
-def solution(skill, skill_trees):
-    cnt = 0
+# import re
+# def solution(skill, skill_trees):
+#     cnt = 0
     
     
-    for skill_tree in skill_trees:
-        res = re.sub("[^" + skill + "]", "", skill_tree)
-        if res in skill and skill.find(res) == 0:
-            cnt += 1
+#     for skill_tree in skill_trees:
+#         res = re.sub("[^" + skill + "]", "", skill_tree)
+#         if res in skill and skill.find(res) == 0:
+#             cnt += 1
         
-    return cnt
+#     return cnt
+
+def solution(skill, skill_trees):
+    answer = 0
+
+    for skills in skill_trees:
+        skill_list = list(skill)
+
+        for s in skills:
+            if s in skill:
+                if s != skill_list.pop(0):
+                    break
+        else:
+            answer += 1
+
+    return answer
