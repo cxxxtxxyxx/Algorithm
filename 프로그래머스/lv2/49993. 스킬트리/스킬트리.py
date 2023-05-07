69e7caf7237caa9d6047f542d490e94321ac05b8
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-07 16:08:51 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -10,15 +10,18 @@
         
 #     return cnt
 
+
+from collections import deque
+
 def solution(skill, skill_trees):
     answer = 0
 
     for skills in skill_trees:
-        skill_list = list(skill)
+        skill_list = deque(skill)
 
         for s in skills:
             if s in skill:
-                if s != skill_list.pop(0):
+                if s != skill_list.popleft():
                     break
         else:
             answer += 1
