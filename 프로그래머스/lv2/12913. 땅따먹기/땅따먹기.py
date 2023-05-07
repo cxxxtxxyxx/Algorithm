431ab7e05322e9f238c5f82f0705360585f45c8d
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-07 15:40:50 +0900

    [level 2] Title: 땅따먹기, Time: 401.92 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -3,9 +3,11 @@ import sys
 def solution(land):
     
     for i in range(1, len(land)):
-        land[i][0] = max(land[i][0] + land[i - 1][1], land[i][0] + land[i - 1][2], land[i][0] + land[i - 1][3])
-        land[i][1] = max(land[i][1] + land[i - 1][0], land[i][1] + land[i - 1][2], land[i][1] + land[i - 1][3])
-        land[i][2] = max(land[i][2] + land[i - 1][0], land[i][2] + land[i - 1][1], land[i][2] + land[i - 1][3])
-        land[i][3] = max(land[i][3] + land[i - 1][0], land[i][3] + land[i - 1][2], land[i][3] + land[i - 1][1])
+        for j in range(4):
+            land[i][j] = max(land[i-1][:j] + land[i-1][j+1:]) + land[i][j]
+        # land[i][0] = max(land[i][0] + land[i - 1][1], land[i][0] + land[i - 1][2], land[i][0] + land[i - 1][3])
+        # land[i][1] = max(land[i][1] + land[i - 1][0], land[i][1] + land[i - 1][2], land[i][1] + land[i - 1][3])
+        # land[i][2] = max(land[i][2] + land[i - 1][0], land[i][2] + land[i - 1][1], land[i][2] + land[i - 1][3])
+        # land[i][3] = max(land[i][3] + land[i - 1][0], land[i][3] + land[i - 1][2], land[i][3] + land[i - 1][1])
                            
     return max(land[-1])
