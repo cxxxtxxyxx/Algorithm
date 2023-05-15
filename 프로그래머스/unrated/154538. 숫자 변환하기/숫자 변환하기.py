ec3fb35e8f38bf9c2cd75e66f35e5fadfeb71ce7
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-15 15:25:53 +0900

    [unrated] Title: 숫자 변환하기, Time: 17.87 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -19,24 +19,43 @@
 #                 dp[i] = min(dp[i], dp[i // 3] + 1)
 #     return dp[y]
 
+# def solution(x, y, n):
+#     answer = 0
+#     s = set()
+#     s.add(x)
+
+#     while s:
+#         if y in s:
+#             return answer
+
+#         nxt = set()
+#         for i in s:
+#             if i+n <= y:
+#                 nxt.add(i+n)
+#             if i*2 <= y:
+#                 nxt.add(i*2)
+#             if i*3 <= y:
+#                 nxt.add(i*3)
+#         s = nxt
+#         answer+=1
+
+#     return -1
+
+from collections import deque
+
 def solution(x, y, n):
-    answer = 0
-    s = set()
-    s.add(x)
+    visited = [0] * 1000001
 
-    while s:
-        if y in s:
-            return answer
-
-        nxt = set()
-        for i in s:
-            if i+n <= y:
-                nxt.add(i+n)
-            if i*2 <= y:
-                nxt.add(i*2)
-            if i*3 <= y:
-                nxt.add(i*3)
-        s = nxt
-        answer+=1
+    q = deque()
+    q.append((x, 0))
+    visited[x] = 1
+    while q:
+        num, cnt = q.popleft()
+        if num == y:
+            return cnt
+        for next_num in (num + n, num * 2, num * 3):
+            if next_num <= y and not visited[next_num]:
+                visited[next_num] = 1
+                q.append((next_num, cnt + 1))
 
     return -1
