7886299cab572f7b0e9b3b94f5902a9781e8d165
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-15 15:53:05 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 110.58 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -4,10 +4,10 @@ def solution(numbers):
     for number in numbers:
         binary_str = "0b" + "0" * 10 + bin(number)[2:]
         if binary_str[-1] == '0':
-            answer.append(int(binary_str[:-1] + '1', 2))
+            answer.append(number + 1)
             continue
-            
-        answer.append(int(binary_str[:binary_str.rfind("01")] + "10" + binary_str[binary_str.rfind("01") + 2:], 2))
+        idx = binary_str.rfind("01")
+        answer.append(int(binary_str[:idx] + "10" + binary_str[idx + 2:], 2))
             
             
     return answer
