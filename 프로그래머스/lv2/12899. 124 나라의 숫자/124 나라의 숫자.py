1c677b2d90c9a27810dde1eb9da9aae4fba20fff
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-17 14:46:08 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -0,0 +1,14 @@
+import string
+
+def solution(n):
+    answer = ''
+    
+    while n:
+        if n % 3 != 0:
+            answer = str(n % 3) + answer
+            n //= 3
+        else:
+            answer = "4" + answer
+            n = n // 3 - 1
+        
+    return answer
