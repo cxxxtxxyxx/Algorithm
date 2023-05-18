16e81a11eac8cfdb2bfa8bb95e6d9af6d13e4961
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-18 14:00:21 +0900

    [level 2] Title: 택배상자, Time: 265.80 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -0,0 +1,13 @@
+def solution(order):
+    
+    stack = []
+    
+    idx = 1
+    count = 0
+    for i in range(1, len(order) + 1):
+        stack.append(i)
+        while stack and stack[-1] == order[count]:
+            count += 1
+            stack.pop()
+            
+    return count
