31867660a9a26a0fdf8be4464dfac34abbd234da
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-22 23:38:14 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,28 +1,92 @@
-import sys
+# import sys
+# def solution(sequence, k):
+#     answer = [0, 0]
+#     _min = sys.maxsize
+    
+#     start = 0
+#     end = 0
+#     _sum = sequence[0]
+#     n = len(sequence)
+#     while end < n:
+#         if _sum == k and _min > end - start + 1:
+#             _min = end - start + 1
+#             answer[0] = start
+#             answer[1] = end
+            
+#         elif _sum > k:
+#             start += 1
+#             _sum -= sequence[start - 1]
+#             continue
+            
+#         end += 1
+#         if end >= n:
+#             break
+#         _sum += sequence[end]
+#     return answer
+
+# def solution(sequence, k):
+#     answer = []
+
+#     start, end = 0, 0
+#     acc = sequence[0]
+
+#     min_len = 2147483647
+#     while start <= end < len(sequence):
+#         if start > end:
+#             acc = sequence[start]
+#             end = start
+#             continue
+
+#         if acc == k:
+#             if min_len > end - start + 1:
+#                 min_len = end - start + 1
+#                 answer = [start, end]
+#             acc -= sequence[start]
+#             start += 1
+#         elif acc > k:
+#             acc -= sequence[start]
+#             start += 1
+#         elif acc < k:
+#             end += 1
+#             if end < len(sequence):
+#                 acc += sequence[end]
+
+#     return answer
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
 def solution(sequence, k):
     answer = []
-    _min = sys.maxsize
-    
-    start = 0
+    # start = 0
     end = 0
-    _sum = sum(sequence[start:end+1])
-    
-    while end < len(sequence):
-        if _sum == k and _min > end - start + 1:
-            _min = end - start + 1
-            answer = [start, end]
+    interval_sum = 0
+    lenth = 10000000
+
+    for start in range(len(sequence)):
+        while interval_sum < k and end < len(sequence):
+            interval_sum += sequence[end]
             end += 1
-            if end >= len(sequence):
-                break
-            _sum += sequence[end]
-        elif _sum > k:
-            start += 1
-            _sum -= sequence[start - 1]
-        else:
-            end += 1
-            if end >= len(sequence):
-                break
-            _sum += sequence[end]
-            
-            
+
+        if interval_sum == k and end - start < lenth:
+            answer = [start,end-1]
+            lenth = end - start
+        interval_sum -= sequence[start]
+
     return answer
