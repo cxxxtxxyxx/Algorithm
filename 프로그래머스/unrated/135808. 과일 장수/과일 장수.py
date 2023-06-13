265417e193e5a2453d95f8c10b5271c4c543f3ab
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-06-13 14:23:01 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,12 @@
+def solution(k, m, score):
+    answer = 0
+    
+    if m > len(score):
+        return 0
+    
+    score.sort()
+    
+    score = score[len(score) % m:]
+    for i in range(0, len(score), m):
+        answer += min(score[i:i+m]) * m
+    return answer
