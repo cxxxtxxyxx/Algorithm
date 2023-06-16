0cae6e65314049fc208e89e8a235b223a6938d16
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-06-16 16:17:26 +0900

    [unrated] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,28 +1,10 @@
 def solution(babbling):
     answer = 0
-    pronounce = {"aya":"b", "ye":"c", "woo":"d", "ma":"f"}
-    change = "bcdf"
-    sequence = []
-    for i in range(2, 16):
-        sequence.append("b" * i)
-        sequence.append("c" * i)
-        sequence.append("d" * i)
-        sequence.append("f" * i)
-        
-    for i in range(len(babbling)):
-        tmp = babbling[i]
-        for p in pronounce.keys():
-            if p in tmp:
-                tmp = tmp.replace(p, pronounce[p])
-        for s in sequence:
-            if s in tmp:
-                break
-        else:
-            for t in tmp:
-                if t not in change:
-                    break
-            else:
-                answer += 1
-
-
+    pronounce = ['aya','ye','woo','ma']
+    for el in babbling:
+        for p in pronounce:
+            if p * 2 not in el:
+                el = el.replace(p,' ')
+        if len(el.strip())==0:
+            answer +=1
     return answer
