fe661e7a2ba085a5d58fa418da57bbdf69c7b5dc
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-11-23 01:15:02 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 476 ms, Memory: 118356 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,38 +1,37 @@
 import sys
-import math
-from functools import reduce
 
 input = sys.stdin.readline
 
-def isPrime(num):
-    if(num == 1):
-        return False
-    for idx in range(2, int(math.sqrt(num)) + 1):
-        if(num % idx == 0):
-            return False
-    return True
+
+def get_primes():
+    MAX_SIZE = 1_000_001
+    primes = [True] * MAX_SIZE
+
+    primes[0] = primes[1] = False
+
+    for i in range(2, MAX_SIZE):
+        if primes[i] == True:
+            for j in range(i * 2, MAX_SIZE, i):
+                primes[j] = False
+
+    return primes
 
 
+primes = get_primes()
 while True:
-    n = int(input())
-    if n == 0:
+
+    N = int(input().strip())
+
+    if N == 0:
         break
 
-    a = 3
-    b = n - a
-
-    while a <= b:
-
-        if isPrime(a) and isPrime(b):
-            print(f'{n} = {a} + {b}')
-            break
-        
-        else:
-            a += 2
-            b -= 2
-
-            if a > b:
-                print("Goldbach's conjecture is wrong.")
+    flag = False
+    for i in range(len(primes)):
+        if primes[i]:
+            if primes[N - i]:
+                print(f'{N} = {i} + {N - i}')
+                flag = True
                 break
-   
 
+    if flag == False:
+        print("Goldbach's conjecture is wrong.")
