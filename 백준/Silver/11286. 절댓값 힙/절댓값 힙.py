034a7ac07ec3c2ad7530dad760a782ce31faa4f3
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2024-01-02 17:54:54 +0900

    [Silver I] Title: 절댓값 힙, Time: 236 ms, Memory: 117468 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,20 +1,23 @@
-import sys
 import heapq
+import sys
 
 input = sys.stdin.readline
 
-n = int(input().strip())
 
-abs_heap = []
+heap = []
 
-for __ in range(n):
-    num_input = int(input().strip())
+N = int(input().strip())
 
-    if num_input == 0:
-        if len(abs_heap) == 0:
+for __ in range(N):
+
+    num = int(input().strip())
+
+    if num == 0:
+        if not heap:
             print(0)
-        else:
-            print(heapq.heappop(abs_heap)[1])
 
+        else:
+            _max = heapq.heappop(heap)
+            print(_max[1])
     else:
-        heapq.heappush(abs_heap, (abs(num_input), num_input))
+        heapq.heappush(heap, (abs(num), num))
