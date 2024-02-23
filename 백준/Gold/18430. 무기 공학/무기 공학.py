35ce39eaf54b3aab4e41592c1150f5cfdaffb872
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2024-02-23 13:04:01 +0900

    [Gold IV] Title: 무기 공학, Time: 368 ms, Memory: 115984 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.py
+++ 백준/Gold/18430. 무기 공학/무기 공학.py
@@ -4,74 +4,87 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
+
 graph = [list(map(int, input().split())) for __ in range(N)]
+visited = [[False] * M for __ in range(N)]
 
 result = 0
-
-visited = [[False] * M for __ in range(N)]
 
 def backtracking(x, y, _sum):
 
     global result
 
     if y == M:
-        y = 0
         x += 1
+        y = 0
 
     if x == N:
         result = max(result, _sum)
         return
     
-    
-    
     if not visited[x][y]:
-        # x, y(중심)가 좌상
+        """
+        ㅁㅁ
+        ㅁ
+        """
         if x + 1 < N and y + 1 < M and not visited[x + 1][y] and not visited[x][y + 1]:
             visited[x][y] = True
             visited[x + 1][y] = True
             visited[x][y + 1] = True
-            _cursum = _sum + 2 * graph[x][y] + graph[x + 1][y] + graph[x][y + 1]
-            backtracking(x, y + 1, _cursum)
+            backtracking(
+                x, y + 1, _sum + 2 * graph[x][y] + graph[x + 1][y] + graph[x][y + 1]
+            )
             visited[x][y] = False
             visited[x + 1][y] = False
             visited[x][y + 1] = False
 
-
-        # 우상
+        """
+        ㅁㅁ
+          ㅁ
+        """
         if x + 1 < N and y - 1 >= 0 and not visited[x + 1][y] and not visited[x][y - 1]:
             visited[x][y] = True
             visited[x + 1][y] = True
             visited[x][y - 1] = True
-            _cursum = _sum + 2 * graph[x][y] + graph[x + 1][y] + graph[x][y - 1]
-            backtracking(x, y + 1, _cursum)
+            backtracking(
+                x, y + 1, _sum + 2 * graph[x][y] + graph[x + 1][y] + graph[x][y - 1]
+            )
             visited[x][y] = False
             visited[x + 1][y] = False
             visited[x][y - 1] = False
 
-        # 좌하
+        """
+        ㅁ
+        ㅁㅁ
+        """
         if x - 1 >= 0 and y + 1 < M and not visited[x - 1][y] and not visited[x][y + 1]:
             visited[x][y] = True
             visited[x - 1][y] = True
             visited[x][y + 1] = True
-            _cursum = _sum + 2 * graph[x][y] + graph[x - 1][y] + graph[x][y + 1]
-            backtracking(x, y + 1, _cursum)
+            backtracking(
+                x, y + 1, _sum + 2 * graph[x][y] + graph[x - 1][y] + graph[x][y + 1]
+            )
             visited[x][y] = False
             visited[x - 1][y] = False
             visited[x][y + 1] = False
 
-        # 우하
+        """
+          ㅁ
+        ㅁㅁ
+        """
         if x - 1 >= 0 and y - 1 >= 0 and not visited[x - 1][y] and not visited[x][y - 1]:
             visited[x][y] = True
             visited[x - 1][y] = True
             visited[x][y - 1] = True
-            _cursum = _sum + 2 * graph[x][y] + graph[x - 1][y] + graph[x][y - 1]
-            backtracking(x, y + 1, _cursum)
+            backtracking(
+                x, y + 1, _sum + 2 * graph[x][y] + graph[x - 1][y] + graph[x][y - 1]
+            )
             visited[x][y] = False
             visited[x - 1][y] = False
             visited[x][y - 1] = False
+
     backtracking(x, y + 1, _sum)
 
 
 backtracking(0, 0, 0)
-
 print(result)
