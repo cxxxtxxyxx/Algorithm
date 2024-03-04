fae80df2328683842012fb6cc1d5c4a328f03661
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2024-03-04 22:01:46 +0900

    [Gold I] Title: 비숍, Time: 932 ms, Memory: 139800 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.py
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -4,56 +4,59 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 
-chess_borad = [list(map(int, input().strip().split())) for __ in range(N)]
+chess = [list(map(int, input().strip().split())) for __ in range(N)]
 
-white_board = [[0] * N for __ in range(N)]
-black_board = [[0] * N for __ in range(N)]
+white_chess = [[0] * N for __ in range(N)]
+black_chess = [[0] * N for __ in range(N)]
 
 for i in range(N):
     for j in range(N):
-        if chess_borad[i][j] == 1:
+        if chess[i][j] == 1:
             if i % 2 == 0 and j % 2 == 0:
-                black_board[i][j] = 1
+                white_chess[i][j] = 1
             
             if i % 2 == 1 and j % 2 == 1:
-                black_board[i][j] = 1
+                white_chess[i][j] = 1
 
             if i % 2 == 0 and j % 2 == 1:
-                white_board[i][j] = 1
-            
-            if i % 2 == 1 and j % 2 == 0:
-                white_board[i][j] = 1
-        
+                black_chess[i][j] = 1
 
-def backtracking(x, y, result, coords, board):
+            if i % 2 == 1 and j % 2 == 0:
+                black_chess[i][j] = 1
+
+result = 0
+
+def backtracking(x, y, coords, board):
+
+    global result
 
     if y == N:
         x += 1
         y = 0
 
     if x == N and y == 0:
-        return max(result, len(coords))
+        result = max(result, len(coords))
+        return
     
-    if board[x][y] == 1:
-        if (x, y) not in coords:
-            if check_coord(x, y, coords):
-                coords.append((x, y))
-                result = max(result, backtracking(x, y + 1, result, coords, board))
-                coords.pop()
-
-        result = max(result, backtracking(x, y + 1, result, coords, board))
-
-    else:
-        result = max(result, backtracking(x, y + 1, result, coords, board))
-    return result
-
-def check_coord(x, y, coords):
+    if board[x][y] == 0:
+        backtracking(x, y + 1, coords, board)
+        return
+    
 
     for px, py in coords:
-        if abs(px - x) == abs(py - y):
-            return False
-        
-    return True
+        if abs(x - px) == abs(y - py):
+            break
 
+    else:
+        coords.append((x, y))
+        backtracking(x, y + 1, coords, board)
+        coords.pop()
 
-print(backtracking(0, 0, 0, [], black_board) + backtracking(0, 0, 0, [], white_board))
+    backtracking(x, y + 1, coords, board)
+
+backtracking(0, 0, [], white_chess)
+_sum = result
+result = 0
+
+backtracking(0, 0, [], black_chess)
+print(_sum + result)
